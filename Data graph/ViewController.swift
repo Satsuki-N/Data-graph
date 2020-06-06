af5e848964983e02968d8ca21a6a3f01750bd227
Satsuki-N <rock_azalea@yahoo.co.jp>
2020-06-06 22:14:41 +0900

    Initial Commit

---
+++ Data graph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data graph
+//
+//  Created by satsuki nakai on 2020/06/06.
+//  Copyright © 2020 satsuki nakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
